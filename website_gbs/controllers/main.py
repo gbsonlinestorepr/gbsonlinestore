056b3172ee0cb2fa387e8c427f6bb945e343420b
bhavik vyas <bhavikvyas1991@gmail.com>
2020-03-13 00:59:08 +0530

    change virtual quantity into hand on quantity on website product details

---
+++ website_gbs/controllers/main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.http import request
+from odoo.tools import ustr
+from odoo.tools.pycompat import izip
+
+class WebsiteSale(WebsiteSale):
+
+    def get_attribute_value_ids(self, product):
+        res = super(WebsiteSale, self).get_attribute_value_ids(product)
+        variant_ids = [r[0] for r in res]
+        for r, variant in izip(res, request.env['product.product'].sudo().browse(variant_ids)):
+             for i in r:
+                 if type(i) == dict :
+                     i.update({'virtual_available': variant.qty_available})
+        return res
+

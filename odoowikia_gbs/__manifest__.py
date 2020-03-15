a0555e188def314acd810bc5bc1993ded698c33b
bhavik vyas <bhavikvyas1991@gmail.com>
2020-03-15 17:49:06 +0530

    add delivery label

---
+++ odoowikia_gbs/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoowikia GBSG',
+    'category': '',
+    'summary': '',
+    'website': 'https://www.odoowikia.com',
+    'author': 'Divya Vyas',
+    'version': '1.0',
+    'description': """GBSG customization""",
+    'depends': [ 'base', 'stock'],
+    'installable': True,
+    'data': [
+             'data/report_paperformat.xml',
+			 'report/delivery_label.xml',
+             'views/report.xml',
+
+            ],
+    'auto_install': True,
+}

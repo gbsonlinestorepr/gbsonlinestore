7918cb78631d6992e8e06205189b6fbfb16cdfa2
bhavik vyas <bhavikvyas1991@gmail.com>
2020-05-02 01:12:11 +0530

    cahnge warning sentence

--- odoowikia_gbs/static/src/js/website_sale.js
+++ odoowikia_gbs/static/src/js/website_sale.js
@@ -3,13 +3,13 @@ odoo.define('odoowikia_gbs.website_sale', function (require) {
         var base = require("web_editor.base");
     var ajax = require('web.ajax');
     var utils = require('web.utils');
-
+    var translation = require('web.translation');
    var rpc = require('web.rpc');
     var core = require('web.core');
     var config = require('web.config');
     require("website.content.zoomodoo");
-    var _t = core._t;
-
+    var _t = translation._t;
+t-options="{'widget': 'monetary', 'from_currency': website_sale_order.pricelist_id.currency_id, 'display_currency': website.currency_id}"
       $(document).on("click","#checkout_button",function(){
             var self = this;
             var div = $("#order_total").children().children().children().text();
@@ -21,7 +21,8 @@ odoo.define('odoowikia_gbs.website_sale', function (require) {
                 if (result){
                     if (Number(div) < Number(result) ){
                            $( "#gbs_para").remove();
-                          $( "#checkout_button").after("<p  class='pull-right mb32 mr8'><h5 id='gbs_para'><strong>Warning! Minimum Purchase prize should be more then " + result +"</strong></h5></p>");
+                          var warning = _t("Warning! Minimum Purchase price should be more than ")
+                          $( "#checkout_button").after("<p  class='pull-right mb32 mr8'><h5 id='gbs_para' ><strong>" + warning + result +"</strong></h5></p>");
 
                           }
 

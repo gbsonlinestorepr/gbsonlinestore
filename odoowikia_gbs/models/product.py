a7fbc704b492b6ead0bc689fc32834254d50e3b9
bhavik vyas <bhavikvyas1991@gmail.com>
2020-06-25 20:04:10 +0530

    solved issue in purchase price decimal accurancy

--- odoowikia_gbs/models/product.py
+++ odoowikia_gbs/models/product.py
@@ -2,6 +2,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
+from odoo.addons import decimal_precision as dp
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    price_unit = fields.Float(string='Unit Price', required=True, digits=dp.get_precision('Purchase Price'))
 
 
 class ProductProduct(models.Model):

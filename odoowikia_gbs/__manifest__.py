4b3944f161094e87bc17e5e654ee6398aaf5f7fa
bhavik vyas <bhavikvyas1991@gmail.com>
2020-06-25 20:06:33 +0530

    solved issue in purchase price decimal accurancy

--- odoowikia_gbs/__manifest__.py
+++ odoowikia_gbs/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'Divya Vyas',
     'version': '1.0',
     'description': """GBSG customization""",
-    'depends': [ 'base', 'stock','sale'],
+    'depends': [ 'base', 'stock','sale','purchase'],
     'installable': True,
     'data': [
              'data/report_paperformat.xml',

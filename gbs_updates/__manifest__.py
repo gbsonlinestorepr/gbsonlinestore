bcbc2b97e51e84b5c508960426681d589e1ef8ff
divya vyas <divyapanchal7594@gmail.com>
2021-01-29 14:37:58 +0530

    add gbs average group

--- gbs_updates/__manifest__.py
+++ gbs_updates/__manifest__.py
@@ -12,6 +12,7 @@ Updates for GBS
     'depends': ['base', 'product', 'sale', 'stock','purchase'],
     'installable': True,
     'data': [
+        'security/gbs_group.xml',
         'views/product_template_view.xml',
         'views/sale_order_view.xml',
         'views/point_of_sale.xml',

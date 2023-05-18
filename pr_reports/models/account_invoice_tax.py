97e04ef67303d604928542df486778b79ac88568
Irizarry123 <132295868+Irizarry123@users.noreply.github.com>
2023-05-18 15:43:45 -0400

    fix account_invoice_tax
    fix account_invoice_tax

--- pr_reports/models/account_invoice_tax.py
+++ pr_reports/models/account_invoice_tax.py
@@ -5,9 +5,9 @@ class AccountInvoiceTax(models.Model):
     _inherit = 'account.tax'
    # _inherit = 'account.invoice.tax'
 
-    invoice_state = fields.Selection(related="account.move.state")
-    invoice_partner_id = fields.Many2one(related='account.move.partner_id')
-    invoice_date = fields.Date(related='account.move.invoice_date', store=True)
+   # invoice_state = fields.Selection(related="account.move.state")
+   # invoice_partner_id = fields.Many2one(related='account.move.partner_id')
+   # invoice_date = fields.Date(related='account.move.invoice_date', store=True)
 
     amount_total = fields.Monetary(string="Tax")
     amount = fields.Monetary(string="Tax")

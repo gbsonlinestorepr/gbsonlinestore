87298b189693e679c0365310d4bd69e5334dd16a
Irizarry123 <132295868+Irizarry123@users.noreply.github.com>
2023-05-18 16:30:01 -0400

    fix account_payment
    fix account_payment

--- gbs_updates_templates/models/account_payment.py
+++ gbs_updates_templates/models/account_payment.py
@@ -4,6 +4,6 @@ from odoo import models, api
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
-    @api.multi
+   # @api.multi
     def do_print_checks(self):
         return self.env.ref('gbs_updates_templates.action_report_payment_receipt_gbs').report_action(self)
